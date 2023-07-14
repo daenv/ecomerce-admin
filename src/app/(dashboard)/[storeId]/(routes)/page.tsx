b69882e5641802a5e76e486115b2d6a5d0ba4c5a
daenv <daenvle69@gmail.com>
2023-07-14 20:31:08 +0700

    14/7/2023

---
+++ src/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -0,0 +1,9 @@
+const DashboardPage = () => {
+   return (
+      <>
+         <div>this is dastboar</div>
+      </>
+   );
+};
+
+export default DashboardPage;
---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,22 @@
+"use client"
+import { UserButton } from '@clerk/nextjs';
+import { Modal } from '../../components/ui/modal';
+import { useEffect } from 'react';
+import { useStoreModal } from '@/hooks/use-store-modal';
+const SetupPage = () => {
+   const isOpen = useStoreModal((state) => state.isOpen);
+   const onOpen = useStoreModal((state) => state.onOpen);
+
+   useEffect(() =>{
+      if(!isOpen){
+         onOpen();
+      }
+   },[isOpen,onOpen]);
+   return (
+      <div className="p-4">
+         Root
+      </div>
+   );
+};
+
+export default SetupPage;
