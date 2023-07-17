e1f130d63c86babd72529826307a6991b37fcde6
daenv <daenvle69@gmail.com>
2023-07-17 13:20:07 +0700

    17/7/2023

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = async () => {
       redirect('/sign-in');
    }
 
-   const store = await prismadb.store.findMany({
+   const stores = await prismadb.store.findMany({
       where: {
          userId,
       },
@@ -20,9 +20,10 @@ const Navbar = async () => {
    return (
       <div className="border-b">
          <div className="flex h-16 items-center px-4">
-            <StoreSwitcher items={store}/>
-            <MainNav />
+            <StoreSwitcher items={stores} />
+            <MainNav className="mx-6 justify-center" />
             <div className="ml-auto flex items-center space-x-4">
+               {/* <ThemeToggle /> */}
                <UserButton afterSignOutUrl="/" />
             </div>
          </div>
