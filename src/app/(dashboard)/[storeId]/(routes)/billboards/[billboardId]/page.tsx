b562c79a73f4d4f62aa5667570b08340210ae1c5
daenv <daenvle69@gmail.com>
2023-07-23 15:25:37 +0700

    23/7/2023

--- src/app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -14,7 +14,6 @@ const BillboardPage = async ({ params }: { params: { billboardId: string } }) =>
         <div className="flex-col">
                 <div className="flex-1 space-y-4 p-8 pt-6">
                     <BillboardForm initialData ={billboard}/>
-               
                 </div>
         </div>
     )
