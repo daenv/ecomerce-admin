6bf7fce1dd6b454aefdc99a18fa1c96ae682f148
daenv <daenvle69@gmail.com>
2023-07-23 10:38:33 +0700

    23/7/2023

--- src/components/ui/image-upload.tsx
+++ src/components/ui/image-upload.tsx
@@ -1,5 +1,6 @@
 'use client';
-import { Trash } from 'lucide-react';
+import { CldUploadWidget } from 'next-cloudinary';
+import { ImagePlus, Trash } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import { Button } from './button';
 
@@ -29,7 +30,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({ disabled, onChange, onRemove,
          <div className="mb-4 flex items-center gap-4">
             {value.map((url) => (
                <div key={url} className="relative w-[200px] h-[200px] rounded-md overflow-hidden">
-                  {/* <div className="z-10 absolute top-2 right-2">
+                  <div className="z-10 absolute top-2 right-2">
                      <Button
                         type="button"
                         onClick={() => onRemove(url)}
@@ -38,10 +39,24 @@ const ImageUpload: React.FC<ImageUploadProps> = ({ disabled, onChange, onRemove,
                      >
                         <Trash className="h-4 w-4" />
                      </Button>
-                  </div> */}
+                  </div>
                </div>
             ))}
          </div>
+         <CldUploadWidget onUpload={onUpload} uploadPreset="t4drjppf">
+            {({ open }) => {
+               const onClick = () => {
+                  open();
+               };
+
+               return (
+                  <Button type="button" disabled={disabled} variant="secondary" onClick={onClick}>
+                     <ImagePlus className="h-4 w-4 mr-2" />
+                     Upload an Image
+                  </Button>
+               );
+            }}
+         </CldUploadWidget>
       </div>
    );
 };
